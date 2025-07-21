6754a37f56d90c31aae91c8deda8486c53676a96
Abid-Hussain81 <dev.abidhussain@gmail.com>
2025-07-21 12:41:27 +0500

    Fixed routing issue and updated Start component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import './App.css'
 import Start from './Components/Start';
 import Quiz from './Components/Quiz';
+import AttemptResult from './Components/attemptResult';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Start />}/>
         <Route path='/quiz' element={<Quiz />}/>
+        <Route path='/AttemptResult' element = {<AttemptResult />}/>
       </Routes>
     </Router>
   )
